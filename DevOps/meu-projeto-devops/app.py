eb33eb1639ed715fcca2c64d3b4ad3ef117c2cce
Matheus <matheusrodd17@gmail.com>
2025-08-26 11:27:30 -0300

    feat: subtrair e teste

---
+++ DevOps/meu-projeto-devops/app.py
@@ -0,0 +1,10 @@
+# app.py
+def somar(a, b):
+    return a + b
+
+def subtrair(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Soma 2 + 3 =", somar(2, 3))
+    print("Subtrai 5 - 2 =", subtrair(5, 2))
--- DevOps/meu-projeto-devops/tests/test_app.py
+++ DevOps/meu-projeto-devops/tests/test_app.py
@@ -1,4 +1,8 @@
-from app import somar
+# tests/test_app.py
+from app import somar, subtrair
 
 def test_somar():
     assert somar(2, 3) == 5
+
+def test_subtrair():
+    assert subtrair(5, 2) == 3
